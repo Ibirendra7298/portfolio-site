9dff0061069a790ef8ef0df5109f9be88fe3d4fa
Birendra Yadav <76553889+Ibirendra7298@users.noreply.github.com>
2020-12-27 21:24:21 +0530

    Add files via upload

---
+++ build/precache-manifest.27f62a280183369d12244a4129df5017.js
@@ -0,0 +1,50 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "aa33be5d52338ecbfddc5b6afbfb5c3d",
+    "url": "/index.html"
+  },
+  {
+    "revision": "fc8bd662d7d426a3dd6d",
+    "url": "/static/css/2.e4346227.chunk.css"
+  },
+  {
+    "revision": "e07ee538db25de979b26",
+    "url": "/static/css/main.54162bf1.chunk.css"
+  },
+  {
+    "revision": "fc8bd662d7d426a3dd6d",
+    "url": "/static/js/2.5686f010.chunk.js"
+  },
+  {
+    "revision": "e1d2b252d73a4c2bc4d6802ca4ba1aa0",
+    "url": "/static/js/2.5686f010.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e07ee538db25de979b26",
+    "url": "/static/js/main.5876d365.chunk.js"
+  },
+  {
+    "revision": "4f2b1704eab49d50853b",
+    "url": "/static/js/runtime-main.19ccc7f6.js"
+  },
+  {
+    "revision": "2ad46b8bc31059573d73c9933349c6de",
+    "url": "/static/media/computervision.2ad46b8b.jpg"
+  },
+  {
+    "revision": "731f0fdbfb4b3a158fbd78c8736c4ff4",
+    "url": "/static/media/deeplearningimg.731f0fdb.jpg"
+  },
+  {
+    "revision": "22964748d4885953feff7945bbe00bb3",
+    "url": "/static/media/mypic.22964748.png"
+  },
+  {
+    "revision": "4f4d861f52c6e732c27557870a9676b2",
+    "url": "/static/media/timeseries.4f4d861f.png"
+  },
+  {
+    "revision": "2ddf177f25db4c78333c34155babe1bf",
+    "url": "/static/media/webdevelopment.2ddf177f.jpeg"
+  }
+]);
